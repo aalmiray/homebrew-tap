ecbb069ad6159e806c11a3275c8b5cbf47737e7b
jreleaserbot <jreleaser@kordamp.org>
2021-07-29 18:35:36 +0200

    app v1.0.0

--- Formula/app.rb
+++ Formula/app.rb
@@ -3,7 +3,7 @@ class App < Formula
   homepage "https://github.com/aalmiray/app"
   version "1.0.0"
   url "https://github.com/aalmiray/app/releases/download/v1.0.0/app-1.0.0.zip"
-  sha256 "f3264dcfc9efd1b4d1649e4d365f8ed42ca0f95423dd62054a2dafdbb471dfa6"
+  sha256 "b8932865872a0ee2a99e6615d6972ad7593b8cc5091c06e15db15f389a6e5555"
   license "Apache-2.0"
 
   bottle :unneeded
