5e548f42d5f2bd3fdfc507db34a28e8262a00948
jreleaserbot <jreleaser@kordamp.org>
2021-10-12 14:31:38 +0200

    app v1.0.0

--- Formula/app.rb
+++ Formula/app.rb
@@ -3,7 +3,7 @@ class App < Formula
   homepage "https://acme.com/wesome-app"
   version "1.0.0"
   url "https://github.com/aalmiray/app/releases/download/v1.0.0/app-1.0.0.zip"
-  sha256 "013edad034fba6a5151adec743528f69c04d410ffb4b8cccb05b58f8395dca0c"
+  sha256 "b1e3fc87b370b7e1e78e3de50837ae75de0570c092b4d578d068d727d1929f02"
   license "Apache-2.0"
 
   bottle :unneeded
