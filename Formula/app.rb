7c6e618f7a191e58960048174cd34313c420bd1f
jreleaserbot <jreleaser@kordamp.org>
2021-11-18 12:12:12 +0100

    app v1.0.0

--- Formula/app.rb
+++ Formula/app.rb
@@ -1,9 +1,9 @@
 class App < Formula
   desc "Sample application"
   homepage "https://acme.com/wesome-app"
-  url "https://github.com/aalmiray/app/releases/download/v1.0.0/app-1.0.0.zip"
   version "1.0.0"
-  sha256 "fb45d542f4dafa05ed888cd269639533e75341788769fd23972067f3900f7df2"
+  url "https://github.com/aalmiray/app/releases/download/v1.0.0/app-1.0.0.zip"
+  sha256 "2cb2c29a3b4237300bd7fd820366e3b317aae38ccad1e48f8ff950867cd94b76"
   license "Apache-2.0"
 
   depends_on "openjdk@8"
