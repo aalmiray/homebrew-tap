8420ea36dcb5c19227848fd032fdbc80b59361cc
jreleaserbot <jreleaser@kordamp.org>
2022-02-15 21:34:49 +0100

    app v1.0.0

--- Formula/app.rb
+++ Formula/app.rb
@@ -1,17 +1,17 @@
-# Generated with JReleaser 1.0.0-M1 at 2022-02-11T13:35:52.702+01:00
+# Generated with JReleaser 1.0.0-M2 at 2022-02-15T21:34:39.334+01:00
 class App < Formula
   desc "Sample application"
   homepage "https://acme.com/wesome-app"
   url "https://github.com/aalmiray/app/releases/download/v1.0.0/app-1.0.0.zip"
   version "1.0.0"
-  sha256 "88286dc9b65a6655a8f3272d631ad4a3f47892c9f3feddf42a264c68618f1d37"
+  sha256 "49667f91105bfebeab76147b656a24a1991e8a30c27e910c9aebf36af683cdc3"
   license "Apache-2.0"
 
   depends_on "openjdk@8"
 
   def install
     libexec.install Dir["*"]
-    bin.install_symlink "#{libexec}/bin/app"
+    bin.install_symlink "#{libexec}/bin/app" => "app"
   end
 
   test do
