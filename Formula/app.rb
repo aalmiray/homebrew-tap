9d7694d8d0ddfd141cf88883e438fbbe4fd1ef7a
jreleaserbot <jreleaser@kordamp.org>
2022-09-14 01:36:02 +0200

    app v1.0.0

--- Formula/app.rb
+++ Formula/app.rb
@@ -1,4 +1,4 @@
-# Generated with JReleaser 1.2.0 at 2022-09-14T01:35:05.577+02:00
+# Generated with JReleaser 1.2.0 at 2022-09-14T01:35:53.45+02:00
 class App < Formula
   desc "Sample application"
   homepage "https://acme.com/awesome-app"
