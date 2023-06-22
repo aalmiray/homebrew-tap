2d3db3c702acef8ba3d73da8331af12089aaf21c
jreleaserbot <jreleaser@kordamp.org>
2023-06-22 16:31:25 +0200

    app v1.0.0

--- Formula/app.rb
+++ Formula/app.rb
@@ -1,12 +1,13 @@
-# Generated with JReleaser 1.6.0-SNAPSHOT at 2023-04-27T15:04:37.585573+02:00
+# Generated with JReleaser 1.6.0 at 2023-06-22T16:31:12.197277+02:00
 class App < Formula
-  desc "app -- Sample Quarkus CLI application"
-  homepage "https://github.com/aalmiray/app"
-  url "https://github.com/aalmiray/app/releases/download/v1.0.0.Aplha1/app-1.0.0.Aplha1-osx-x86_64.zip"
-  version "1.0.0.Aplha1"
-  sha256 "85c9918b23e3ac4ef64d5dd02714e241231d3f1358afdba09d3fd0b9a889e131"
+  desc "Sample application"
+  homepage "https://acme.com/awesome-app"
+  url "https://github.com/aalmiray/app/releases/download/v1.0.0/app-1.0.0.zip"
+  version "1.0.0"
+  sha256 "7a55a38531d0ab21559fb1dad0ab19251cb39c6dbfba95e4e8cb270acdff1528"
   license "Apache-2.0"
 
+  depends_on "openjdk@8"
 
   def install
     libexec.install Dir["*"]
@@ -15,6 +16,6 @@ class App < Formula
 
   test do
     output = shell_output("#{bin}/app --version")
-    assert_match "1.0.0.Aplha1", output
+    assert_match "1.0.0", output
   end
 end
